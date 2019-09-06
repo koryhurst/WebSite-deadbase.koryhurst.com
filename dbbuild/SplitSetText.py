ad68dbb081acb78af4c8997c9906cc65105791d4
Kory Hurst <koryhurst@gmail.com>
2019-09-06 15:36:04 -0700

    Started Using SetText as a parameter

--- dbbuild/SplitSetText.py
+++ dbbuild/SplitSetText.py
@@ -12,19 +12,23 @@ for OrigLine in OldFileLines:
     ShowID = OrigLine[0:OrigLine.find(",")]
     LinesAddedToThisShow = 0
   LineNr = OrigLine[OrigLine.find(",")+1:OrigLine.find(",",OrigLine.find(",")+1)] #weird way of finding the second one.
+  SetText = OrigLine[len(ShowID) + len(LineNr) + 2:len(OrigLine)]
   print("-------------")
-  print(OrigLine)
-  print(ShowID)
-  print(LineNr)
+  print("OrigText: " + OrigLine)
+  print("ShowID: " + ShowID)
+  print("LineNr: " + LineNr)
+  print("SetText: " + SetText)
   for SplitPoint in SplitPoints:
-    if OrigLine.find(SplitPoint) > 0:
+    # I think this automatically only finds ones after the first one
+    if SetText.find(SplitPoint) > 1:
+      print("SplitPoint: " + str(SetText.find(SplitPoint)))
       ThisSplitPoint = SplitPoint
       #print(OrigLine)
       print("Found Split Point")
       #is it not at the start of the set text
       if OrigLine.find(SplitPoint) != len(ShowID) + len(LineNr) + 3:
         print(len(ShowID) + len(LineNr) + 3)
-        OldLinePart1 = OrigLine[0:OrigLine.find(ThisSplitPoint)-1] + '"'
+        OldLinePart1 = OrigLine[0:OrigLine.find(ThisSplitPoint)-1] + '"\\r\\n'
         print("OldLinePart1: " + OldLinePart1)
         OldLinePart2 = ShowID + ',' + str((int(LineNr) + 1)) + ',"' + OrigLine[OrigLine.find(ThisSplitPoint):len(OrigLine)]
         print("OldLinePart2: " + OldLinePart2)
@@ -33,7 +37,7 @@ for OrigLine in OldFileLines:
         NewSetList.write(OldLinePart2)
         wait = input("PRESS ENTER TO CONTINUE")
     else:
-      NewSetList.write(ShowID + "," + str(int(LineNr) + LinesAddedToThisShow))
+      NewSetList.write(ShowID + "," + str(int(LineNr) + LinesAddedToThisShow) + "," + SetText)
   
   
 #      1.5:
