2b91e0bade07031ae050f31237ecbe74226109f1
Kory Hurst <koryhurst@gmail.com>
2019-09-06 14:54:41 -0700

    A bunch of general fixes

--- dbbuild/SplitSetText.py
+++ dbbuild/SplitSetText.py
@@ -4,10 +4,13 @@ NewSetList = open("NewSetList.csv", "w+")
 OldSetList = open("setlist.csv", "r")
 
 SplitPoints = ["1.5:","2:","2.5:","E:","E1:","E2:"]
-
+ShowID = 0
+LinesAddedToThisShow = 0
 OldFileLines = OldSetList.readlines()
 for OrigLine in OldFileLines:
-  ShowID = OrigLine[0:OrigLine.find(",")]
+  if OrigLine[0:OrigLine.find(",")] != ShowID:  
+    ShowID = OrigLine[0:OrigLine.find(",")]
+    LinesAddedToThisShow = 0
   LineNr = OrigLine[OrigLine.find(",")+1:OrigLine.find(",",OrigLine.find(",")+1)] #weird way of finding the second one.
   print("-------------")
   print(OrigLine)
@@ -18,11 +21,15 @@ for OrigLine in OldFileLines:
       ThisSplitPoint = SplitPoint
       #print(OrigLine)
       print("Found Split Point")
-      OldLinePart1 = OrigLine[0:OrigLine.find(ThisSplitPoint)] + '"'
-      print("OldLinePart1: " + OldLinePart1)
-      OldLinePart2 = ShowID + ',' + str((int(LineNr) + 1)) + ',' + OrigLine[OrigLine.find(ThisSplitPoint):len(OrigLine)]
-      print("OldLinePart2: " + OldLinePart2)
-      wait = input("PRESS ENTER TO CONTINUE")
+      #is it not at the start of the set text
+      if OrigLine.find(SplitPoint) != len(ShowID) + len(LineNr) + 3:
+        print(len(ShowID) + len(LineNr) + 3)
+        OldLinePart1 = OrigLine[0:OrigLine.find(ThisSplitPoint)-1] + '"'
+        print("OldLinePart1: " + OldLinePart1)
+        OldLinePart2 = ShowID + ',' + str((int(LineNr) + 1)) + ',"' + OrigLine[OrigLine.find(ThisSplitPoint):len(OrigLine)]
+        print("OldLinePart2: " + OldLinePart2)
+        LinesAddedToThisShow = LinesAddedToThisShow + 1
+        wait = input("PRESS ENTER TO CONTINUE")
   
   
 #      1.5:
