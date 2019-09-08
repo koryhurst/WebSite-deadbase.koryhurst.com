2e4a2d25b1f30e41b8ac8f4df5d317ba2d85328b
Kory Hurst <koryhurst@gmail.com>
2019-09-07 18:41:11 -0700

    I think I got it working

--- dbbuild/SplitSetText.py
+++ dbbuild/SplitSetText.py
@@ -1,13 +1,16 @@
 import csv
 
-NewSetList = open("NewSetList.csv", "a+")
+NewSetList = open("NewSetList.csv", "w+")
+NewSetList = open("NewSetList.csv", "a")
 OldSetList = open("setlist.csv", "r")
 
-SplitPoints = ["1.5:","2:","2.5:","E:","E1:","E2:"]
+SplitPoints = ["Electric-2:", "1.5:","2:","2.5:","E:","E1:","E2:"]
 ShowID = 0
 LinesAddedToThisShow = 0
+
 OldFileLines = OldSetList.readlines()
 for OrigLine in OldFileLines:
+  SplitLinesOutput = False
   if OrigLine[0:OrigLine.find(",")] != ShowID:  
     ShowID = OrigLine[0:OrigLine.find(",")]
     LinesAddedToThisShow = 0
@@ -19,31 +22,33 @@ for OrigLine in OldFileLines:
   print("LineNr: " + LineNr)
   print("SetText: " + SetText)
   for SplitPoint in SplitPoints:
-    # I think this automatically only finds ones after the first one
-    if SetText.find(SplitPoint) > 1:
-      print("SplitPoint: " + str(SetText.find(SplitPoint)))
-      ThisSplitPoint = SplitPoint
-      #print(OrigLine)
+    
+    
+    #first is there a split point in there
+    if SetText.find(SplitPoint) > 12:
       print("Found Split Point")
+      print("SplitPoint: " + str(SetText.find(SplitPoint)))
+      #ThisSplitPoint = SplitPoint
+      #print(OrigLine)
       #is it not at the start of the set text
-      if OrigLine.find(SplitPoint) != len(ShowID) + len(LineNr) + 3:
-        print(len(ShowID) + len(LineNr) + 3)
-        OldLinePart1 = OrigLine[0:OrigLine.find(ThisSplitPoint)-1] + '"\\r\\n'
+      #if OrigLine.find(SplitPoint) != len(ShowID) + len(LineNr) + 3:
+
+
+
+      if SetText[0:len(SplitPoint)] != SplitPoint:
+
+
+
+        print(SetText[0:len(SplitPoint)])
+        OldLinePart1 = OrigLine[0:OrigLine.find(SplitPoint)-1] + '\n'
         print("OldLinePart1: " + OldLinePart1)
-        OldLinePart2 = ShowID + ',' + str((int(LineNr) + 1)) + ',"' + OrigLine[OrigLine.find(ThisSplitPoint):len(OrigLine)]
+        OldLinePart2 = ShowID + ',' + str((int(LineNr) + 1)) + ',"' + OrigLine[OrigLine.find(SplitPoint):len(OrigLine)]
         print("OldLinePart2: " + OldLinePart2)
         LinesAddedToThisShow = LinesAddedToThisShow + 1
         NewSetList.write(OldLinePart1)
         NewSetList.write(OldLinePart2)
-        wait = input("PRESS ENTER TO CONTINUE")
-    else:
-      NewSetList.write(ShowID + "," + str(int(LineNr) + LinesAddedToThisShow) + "," + SetText)
-  
-  
-#      1.5:
-#      2:
-#      2.5:
-#      E:
-#      E1:
-#      E2:
-
+        #wait = input("PRESS ENTER TO CONTINUE")
+        SplitLinesOutput = True
+  if SplitLinesOutput == False:
+    NewSetList.write(ShowID + "," + str(int(LineNr) + LinesAddedToThisShow) + "," + SetText)
+    
